4b4fe1fe2bc621fb5bdda1471b21e125023a2e01
ryndoa <ryndoa@soyuz317.ru>
2022-06-20 11:31:03 +0300

    SR-4504.Завершенные КП можно заново взять в работу(BUGfix 3) https://jira.smartro.ru/browse/SR-4504

--- app/src/main/java/ru/smartro/worknote/work/MapActBottomBehaviorAdapter.kt
+++ app/src/main/java/ru/smartro/worknote/work/MapActBottomBehaviorAdapter.kt
@@ -115,11 +115,9 @@ class MapActBottomBehaviorAdapter(
                             holder.itemView.map_behavior_start_service.setText(R.string.start_serve_again)
                         }
                         holder.itemView.map_behavior_start_service.setOnClickListener {
-                            lastHolder?.collapseOld()
                             listener.startPlatformService(item)
                         }
                         holder.itemView.map_behavior_fire.setOnClickListener {
-                            lastHolder?.collapseOld()
                             listener.startPlatformProblem(item)
                         }
 //                        lastHolder?.let {
@@ -161,6 +159,9 @@ class MapActBottomBehaviorAdapter(
 
     class PlatformViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun collapseOld() {
+            if (platformId == null) {
+                return
+            }
             itemView.map_behavior_expl?.collapse()
             platformId = null
         }
