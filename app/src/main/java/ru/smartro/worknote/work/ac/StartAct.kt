9cffe4657906f5cd58243e90f9681663f9639147
ryndoa <ryndoa@soyuz317.ru>
2022-08-05 17:43:56 +0300

    Android_Serv clean architecture g79015884904@gmail.com

--- app/src/main/java/ru/smartro/worknote/work/ac/StartAct.kt
+++ app/src/main/java/ru/smartro/worknote/work/ac/StartAct.kt
@@ -192,12 +192,21 @@ class StartAct : ActAbstract() {
             }
         }
 //
-        if (isDevelMode()) {
-            authLoginEditText?.setText("admin@smartro.ru")
-            authPasswordEditText?.setText("xot1ieG5ro~hoa,ng4Sh")
-            clickAuthEnter()
+        if (BuildConfig.BUILD_TYPE != "debugProd") {
+            if (isDevelMode()) {
+                authLoginEditText?.setText("g79015884904@gmail.com")
+                authPasswordEditText?.setText("Grafik+76")
+            }
         }
 
+        if (BuildConfig.BUILD_TYPE != "release") {
+            if (isDevelMode()) {
+                authLoginEditText?.setText("admin@smartro.ru")
+                authPasswordEditText?.setText("xot1ieG5ro~hoa,ng4Sh")
+                clickAuthEnter()
+            }
+        }
+//
 
 /*        auth_enter.setOnLongClickListener {
             auth_login.setText("gkh2@smartro.ru")
