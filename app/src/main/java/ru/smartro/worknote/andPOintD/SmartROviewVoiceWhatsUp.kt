08fa0fd3836139ad203ce24bd409d27f2eb4c94f
Vlad Topchu <topchu@soyuz317.ru>
2022-10-06 13:10:17 +0300

    SR-5456.Записывать голосовые сообщения на КП, в качестве коммента https://jira.smartro.ru/browse/SR-5456 refactoring (2 of 3)

--- app/src/main/java/ru/smartro/worknote/andPOintD/SmartROviewVoiceWhatsUp.kt
+++ app/src/main/java/ru/smartro/worknote/andPOintD/SmartROviewVoiceWhatsUp.kt
@@ -4,8 +4,6 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Handler
 import android.os.Looper
-import android.os.VibrationEffect
-import android.os.Vibrator
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
@@ -25,13 +23,6 @@ class SmartROviewVoiceWhatsUp @JvmOverloads constructor(
     defStyleAttrs: Int = 0
 ): ConstraintLayout(context, attrs, defStyleAttrs) {
 
-//    var currentState = VoiceCommentState.IDLE
-
-    private var isLockReady: Boolean = false
-
-    private var DRAG_EDGE_LOCK = 0f
-    private var DRAG_EDGE_CANCEL = 0f
-
     private var messageInput: AppCompatEditText? = null
     private var recordInfo: LinearLayoutCompat? = null
     private var recordTime: AppCompatTextView? = null
@@ -42,7 +33,75 @@ class SmartROviewVoiceWhatsUp @JvmOverloads constructor(
     private var rlPathCancel: RelativeLayout? = null
     private var rlPathLock: RelativeLayout? = null
 
-    var listener: CommentInputEvents? = null
+    private var movableView: MovableView? = null
+
+    private val rlPathCancelX: Int by lazy { ViewUtil(rlPathCancel!!).getXStart() }
+    private val rlPathLockX: Int by lazy { ViewUtil(rlPathLock!!).getXStart() }
+
+    private var mIsLockReady = false
+
+    init {
+        inflate(getContext(), R.layout.custom_view__comment_input, this)
+
+        rlPathCancel = findViewById(R.id.rl__comment_input__path_cancel)
+        rlPathLock = findViewById(R.id.rl__comment_input__path_lock)
+        messageInput = findViewById(R.id.acet__comment_input__message_input)
+        recordInfo = findViewById(R.id.llc__comment_input__record_info)
+        recordTime = findViewById(R.id.actv__comment_input__record_time)
+        recordButton = findViewById(R.id.aciv__comment_input__rec_start)
+        recordButtonWrapper = findViewById(R.id.cl__comment_input__record_button_wrapper)
+        buttonStop = findViewById(R.id.aciv__comment_input__stop_button)
+        buttonCancel = findViewById(R.id.actv__comment_input__cancel_button)
+
+        initialState()
+
+        buttonStop?.setOnClickListener {
+            // TODO: onStop()
+            initialState()
+        }
+
+        buttonCancel?.setOnClickListener {
+            // TODO: onCancel()
+            initialState()
+        }
+
+        movableView = MovableViewBuilder()
+            .setTargetView(recordButton!!)
+            .setMovementRules(MovableViewBuilder.MovementRule.LEFT)
+            .setDelay(200)
+            .onStart {
+                LOG.debug("MOVABLE ON START")
+                recordingState()
+                recordButton?.animate()?.scaleX(1.6f)?.scaleY(1.6f)?.setDuration(200)?.start()
+            }
+            .onMoveHorizontally { view, absoluteX ->
+                LOG.debug("MOVABLE ON MOVE HORIZONTALLY: ${absoluteX}")
+                if(!mIsLockReady && absoluteX > (rlPathLockX - 30f) && absoluteX < (rlPathLockX + 30f)) {
+                    LOG.debug("LOCK")
+                    mIsLockReady = true
+                }
+
+                if(mIsLockReady && absoluteX < (rlPathLockX - 50f) || absoluteX > (rlPathLockX + 50f)) {
+                    LOG.debug("UNLOCK")
+                    mIsLockReady = false
+                }
+
+                if(absoluteX < rlPathCancelX) {
+                    LOG.debug("CANCEL")
+                    movableView?.stopMovement()
+                }
+            }
+            .onStop {
+                LOG.debug("MOVABLE ON STOP")
+                recordButton?.animate()?.scaleX(1f)?.scaleY(1f)?.setDuration(200)?.start()
+                if(mIsLockReady) {
+                    lockState()
+                } else {
+                    initialState()
+                }
+            }
+            .build()
+    }
 
     fun setTime(timeInMS: Long) {
         val minutes = timeInMS / 60_000
@@ -52,7 +111,11 @@ class SmartROviewVoiceWhatsUp @JvmOverloads constructor(
         recordTime?.text = "${minutesShowForUser}:${secondsShowForUser}"
     }
 
-    private fun initState() {
+    fun stop() {
+
+    }
+
+    private fun initialState() {
         recordButtonWrapper?.visibility = VISIBLE
         messageInput?.visibility = VISIBLE
 
@@ -64,189 +127,38 @@ class SmartROviewVoiceWhatsUp @JvmOverloads constructor(
         buttonCancel?.visibility = GONE
     }
 
-    init {
-        inflate(getContext(), R.layout.custom_view__comment_input, this)
+    private fun recordingState() {
+        recordButtonWrapper?.visibility = VISIBLE
+        messageInput?.visibility = INVISIBLE
 
-        rlPathCancel = findViewById(R.id.rl__comment_input__path_cancel)
-        rlPathLock = findViewById(R.id.rl__comment_input__path_lock)
+        recordInfo?.visibility = VISIBLE
+        rlPathCancel?.visibility = VISIBLE
+        rlPathLock?.visibility = VISIBLE
 
-        messageInput = findViewById(R.id.acet__comment_input__message_input)
-        recordInfo = findViewById(R.id.llc__comment_input__record_info)
-        recordTime = findViewById(R.id.actv__comment_input__record_time)
-        recordButton = findViewById(R.id.aciv__comment_input__rec_start)
-        recordButtonWrapper = findViewById(R.id.cl__comment_input__record_button_wrapper)
-        buttonStop = findViewById(R.id.aciv__comment_input__stop_button)
-        buttonCancel = findViewById(R.id.actv__comment_input__cancel_button)
-
-        initState()
-
-        val movableView = MovableViewBuilder()
-            .setTargetView(recordButton!!)
-            .setMovementRules(MovableViewBuilder.MovementRule.LEFT)
-            .setDelay(200)
-            .onStart {
-                LOG.debug("MOVABLE ON START")
-                recordButton?.animate()?.scaleX(1.6f)?.scaleY(1.6f)?.setDuration(200)?.start()
-            }
-            .onMoveHorizontally { view, absoluteX ->
-                LOG.debug("MOVABLE ON MOVE HORIZONTALLY: ${absoluteX}")
-            }
-            .onStop {
-                LOG.debug("MOVABLE ON STOP")
-                recordButton?.animate()?.scaleX(1f)?.scaleY(1f)?.start()
-            }
-            .build()
+        buttonStop?.visibility = GONE
+        buttonCancel?.visibility = GONE
     }
 
-    interface CommentInputEvents {
-        fun onStart()
-        fun onStop()
-        fun onCancel()
-        fun onLock()
+    private fun lockState() {
+        recordButtonWrapper?.visibility = INVISIBLE
+        messageInput?.visibility = INVISIBLE
+
+        recordInfo?.visibility = VISIBLE
+        rlPathCancel?.visibility = INVISIBLE
+        rlPathLock?.visibility = INVISIBLE
+
+        buttonStop?.visibility = VISIBLE
+        buttonCancel?.visibility = VISIBLE
     }
 
-//    override fun onTouch(v: View?, event: MotionEvent): Boolean {
-//        when (event.action) {
-//            MotionEvent.ACTION_DOWN -> {
-//                // Corners of button has the same size
-//                val recordButtonSize = recordButton?.width ?: 0
-//                val location = IntArray(2)
-//                recordButton?.getLocationOnScreen(location)
-//                recordButtonX1 = location[0].toFloat()
-//                recordButtonX2 = recordButtonX1 + recordButtonSize
-//                recordButtonY1 = location[1].toFloat()
-//                recordButtonY2 = recordButtonY1 + recordButtonSize
-//
-//                rlPathCancel?.visibility = View.VISIBLE
-//                rlPathLock?.visibility = View.VISIBLE
-//
-//                recordButton?.translationY = 0f
-//                recordButton?.apply {
-//                    translationX = 0f
-//                    animate().scaleX(1.8f).scaleY(1.8f).setDuration(200).start()
-//                }
-//
-//                vibrator?.vibrate(vibrationEffect)
-//
-//                recordInfo?.visibility = VISIBLE
-//                messageInput?.visibility = INVISIBLE
-//
-//                rlPathCancel?.getLocationOnScreen(location)
-//                DRAG_EDGE_CANCEL = location[0].toFloat()
-//
-//                rlPathLock?.getLocationOnScreen(location)
-//                DRAG_EDGE_LOCK = location[0].toFloat()
-//
-//                currentState = VoiceCommentState.RECORDING
-//                listener?.onStart()
-//
-//                return true
-//            }
-//
-//            MotionEvent.ACTION_MOVE -> {
-//                if (currentState == VoiceCommentState.RECORDING) {
-//
-//                    val rawX = event.rawX
-//                    val rawY = event.rawY
-//
-//                    if (movement == MovementEnum.NONE) {
-//                        if (rawY > recordButtonY1 && rawY < recordButtonY2 && rawX < recordButtonX1) {
-//                            recordButton?.translationX = 0f
-//                            movement = MovementEnum.HORIZONTAL
-//                        }
-//                    }
-//
-//                    if (movement == MovementEnum.HORIZONTAL) {
-//                        dx = rawX - recordButtonX1
-//
-//                        if (dx < 10f) {
-//                            recordButton?.translationX = dx
-//                        }
-//
-//                        if ((rawX > (DRAG_EDGE_LOCK - 30f) && rawX <= DRAG_EDGE_LOCK) && !isLockReady) {
-//                            isLockReady = true
-//                            vibrator!!.vibrate(vibrationEffect)
-//                        }
-//
-//                        if((rawX < (DRAG_EDGE_LOCK - 60f) || rawX > (DRAG_EDGE_LOCK + 30f)) && isLockReady) {
-//                            isLockReady = false
-//                        }
-//
-//                        if (rawX < DRAG_EDGE_CANCEL) {
-//                            currentState = VoiceCommentState.CANCEL
-//
-//                            vibrator!!.vibrate(vibrationEffect)
-//
-//                            val cancelEvent = MotionEvent.obtain(event)
-//                            cancelEvent.action = MotionEvent.ACTION_UP
-//                            recordButton?.dispatchTouchEvent(cancelEvent)
-//
-//                            return false
-//                        }
-//
-//                        if (rawX > recordButtonX1 && rawX < recordButtonX2 && rawY < recordButtonY1) {
-//                            movement = MovementEnum.NONE
-//                        }
-//
-//                        return false
-//                    }
-//                }
-//                return false
-//            }
-//
-//            MotionEvent.ACTION_UP -> {
-//                if(isLockReady) {
-//                    listener?.onLock()
-//                    currentState = VoiceCommentState.LOCK
-//
-//                    recordButtonWrapper?.visibility = INVISIBLE
-//                    buttonStop?.visibility = VISIBLE
-//                    buttonCancel?.visibility = VISIBLE
-//                }
-//
-//                recordButton?.apply {
-//                    animate()
-//                        .setDuration(200)
-//                        .scaleX(1f).scaleY(1f)
-//                        .translationX(0f).translationY(0f)
-//                        .start()
-//                }
-//
-//                movement = MovementEnum.NONE
-//
-//                rlPathCancel?.visibility = View.INVISIBLE
-//                rlPathLock?.visibility = View.INVISIBLE
-//
-//                if(currentState == VoiceCommentState.CANCEL) {
-//                    // animate trashcan
-//                    // and then ->
-//                    listener?.onCancel()
-//                    currentState = VoiceCommentState.IDLE
-//
-//                    initState()
-//                    return false
-//                }
-//
-//                if(currentState == VoiceCommentState.RECORDING) {
-//                    listener?.onStop()
-//                    currentState = VoiceCommentState.IDLE
-//
-//                    initState()
-//                }
-//
-//                return false
-//            }
-//        }
-//        return false
-//    }
 }
 
 class MovableView(
     val targetView: View,
-    private var mOnStop: ((view: View) -> Unit)? = null
+    private var mOnStop: (() -> Unit)? = null
 ) {
     fun stopMovement() {
-        mOnStop?.invoke(targetView)
+        mOnStop?.invoke()
     }
 }
 
@@ -270,9 +182,9 @@ class MovableViewBuilder {
     private var mIsRuleRightInitialized: Boolean = false
     private var mIsRuleBottomInitialized: Boolean = false
 
-    private val mVerticalEdges = mutableListOf<EdgeSetting>()
-
     private var mIsReadyForMove = false
+
+    private var mCurrentMotionEvent: MotionEvent? = null
 
     fun setTargetView(targetView: View): MovableViewBuilder {
         mTargetView = targetView
@@ -320,6 +232,13 @@ class MovableViewBuilder {
         mOnStart?.invoke(mTargetView!!)
     }
 
+    @SuppressLint("Recycle")
+    private fun cancel() {
+        val actionUpEvent = MotionEvent.obtain(mCurrentMotionEvent)
+        actionUpEvent.action = MotionEvent.ACTION_UP
+        mTargetView?.dispatchTouchEvent(actionUpEvent)
+    }
+
     private fun stop() {
         mIsReadyForMove = false
         mTargetView?.animate()?.translationX(0f)?.translationY(0f)?.start()
@@ -335,6 +254,7 @@ class MovableViewBuilder {
 //            var viewXEnd: Int
 
         mTargetView!!.setOnTouchListener { _, event ->
+            mCurrentMotionEvent = event
             when(event.action) {
                 MotionEvent.ACTION_DOWN -> {
                     mTargetView?.isPressed = true
@@ -384,14 +304,8 @@ class MovableViewBuilder {
             }
         }
 
-        return MovableView(mTargetView!!, mOnStop)
+        return MovableView(mTargetView!!, ::cancel)
     }
-
-    data class EdgeSetting (
-        val edge: Int,
-        val isDeadly: Boolean = false,
-        val expandTo: Int? = null
-    )
 
     enum class MovementRule {
         LEFT, TOP, RIGHT, BOTTOM
